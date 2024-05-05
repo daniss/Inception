e0fbe6a87afe828684a45a685bb38618127ec443
daniss <daniscin@gmail.com>
2024-05-06 01:06:33 +0200

    Finished +bullseye

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-service mysql start;
+service mariadb start;
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;";
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;";
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';";
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';";
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';";
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';";
 
-mysql -uroot -p"$SQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+mariadb -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO 'root'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysqladmin -uroot -p$SQL_ROOT_PASSWORD shutdown;
+mariadb -e "FLUSH PRIVILEGES;";
 
-exec mysqld_safe;
+mysqladmin shutdown;
+
+mysqld_safe;
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
-sleep 10;
-if [ -d '/var/www/html' ]; then
-    :
-else
-    mkdir /var/www/html;
-    mv /var/www/wordpress/* /var/www/html;
-    rm -rf /var/www/wordpress;
-    wp-cli.phar config create	--allow-root \
-    			--dbname=$SQL_DATABASE \
-    			--dbuser=$SQL_USER \
-    			--dbpass=$SQL_PASSWORD \
-    			--dbhost=$SQL_HOST --path='/var/www/html'
-    wp-cli.phar core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path=/var/www/html;
+sleep 10
 
-    wp-cli.phar user create --allow-root $WP_USER2 $WP_EMAIL2 --user_pass=$WP_PASSWORD2 --role=author --path=/var/www/html;
-
-    sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"      /var/www/html/wp-config.php
-    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               /var/www/html/wp-config.php
-    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            /var/www/html/wp-config.php
-fi
-
-if [ -d '/run/php' ]; then
-    :
-else
-    mkdir /run/php;
-fi
+wp-cli.phar config create --allow-root \
+			--dbname=$SQL_DATABASE \
+			--dbuser=$SQL_USER \
+			--dbpass=$SQL_PASSWORD \
+			--dbhost=$SQL_HOST --path='/var/www/wordpress'
+wp-cli.phar core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path="/var/www/wordpress";
+wp-cli.phar user create --allow-root $WP_USER2 $WP_EMAIL2 --user_pass=$WP_PASSWORD2 --role=author --path="/var/www/wordpress";
+sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"      /var/www/wordpress/wp-config.php
+sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               /var/www/wordpress/wp-config.php
+sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            /var/www/wordpress/wp-config.php
 
 
-exec php-fpm7.3 -F
+exec php-fpm7.4 -F

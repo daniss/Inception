efbdc241bc79dec735e4d9da1b4912e61fac77c2
daniss <daniscin@gmail.com>
2024-06-06 17:15:27 +0200

    Open port fixed

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -9,6 +9,7 @@ wp-cli.phar config create --allow-root \
 			--dbhost=$SQL_HOST --path='/var/www/wordpress'
 wp-cli.phar core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path="/var/www/wordpress";
 wp-cli.phar user create --allow-root $WP_USER2 $WP_EMAIL2 --user_pass=$WP_PASSWORD2 --role=author --path="/var/www/wordpress";
+chown -R www-data:www-data /var/www/wordpress
 sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"      /var/www/wordpress/wp-config.php
 sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               /var/www/wordpress/wp-config.php
 sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            /var/www/wordpress/wp-config.php
